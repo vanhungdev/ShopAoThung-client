4083b7db890d501bc7934d41d5bc67a91d413e58
vanhungdev <vanhungdev@gmail.com>
2020-10-28 16:20:24 +0700

    new commit

--- ShopAoThung/App_Start/RouteConfig.cs
+++ ShopAoThung/App_Start/RouteConfig.cs
@@ -12,6 +12,32 @@ namespace ShopAoThung
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //can doi
+            routes.MapRoute(
+            name: "huy thanh toan online",
+            url: "cancel-order",
+            defaults: new { controller = "Checkout", action = "cancel_order", id = UrlParameter.Optional },
+            new[] { "ShopAoThung.Controllers" }
+            );
+            routes.MapRoute(
+           name: "thanh toan thanh cong",
+           url: "confirm-orderPaymentOnline",
+           defaults: new { controller = "Checkout", action = "confirm_orderPaymentOnline", id = UrlParameter.Optional },
+           new[] { "ShopAoThung.Controllers" }
+           );
+            routes.MapRoute(
+           name: "huy thanh toan online momo",
+           url: "cancel-order-momo",
+           defaults: new { controller = "Checkout", action = "cancel_order_momo", id = UrlParameter.Optional },
+           new[] { "ShopAoThung.Controllers" }
+           );
+
+            routes.MapRoute(
+           name: "thanh toan thanh cong momo",
+           url: "confirm-orderPaymentOnline-momo",
+           defaults: new { controller = "Checkout", action = "confirm_orderPaymentOnline_momo", id = UrlParameter.Optional },
+           new[] { "ShopAoThung.Controllers" }
+           );
 
             routes.MapRoute(
         name: "thanh-toan",
